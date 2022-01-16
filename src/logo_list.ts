6141c388f6ba9a2aaa69b7c7d512385de28e9873
dan4648 <wakwaujaim@gmail.com>
2022-01-16 22:16:52 +0700

    feat: added logo list

---
+++ src/logo_list.ts
@@ -0,0 +1,8 @@
+import cleverSvg from './images/clever_logo.svg';
+import diamondSvg from './images/diamond_logo.svg';
+import financeSvg from './images/finance_logo.svg';
+import gambloSvg from './images/gamblo_logo.svg';
+
+const logoList: string[] = [cleverSvg, diamondSvg, financeSvg, gambloSvg];
+
+export default logoList;
